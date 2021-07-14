b09736ec675b205879c21a218a62e35cc85fd019
gui-andrades <guilherme.andrade@dtidigital.com.br>
2021-07-14 17:56:42 -0300

    Atualização dos modelos de dados de chamado

--- POCJira.Repository/Entities/ChamadoEntity.cs
+++ POCJira.Repository/Entities/ChamadoEntity.cs
@@ -7,5 +7,13 @@ namespace POCJira.Repository.Entities
         public long Id { get; set; }
 
         public String Status { get; set; }
+
+        public String Nome { get; set; }
+
+        public String Descricao { get; set; }
+
+        public long Valor { get; set; }
+
+        public String Responsavel { get; set; }
     }
 }
--- POCJira.WebApi/DTOs/Responses/ChamadoDto.cs
+++ POCJira.WebApi/DTOs/Responses/ChamadoDto.cs
@@ -7,5 +7,13 @@ namespace POCJira.WebApi.DTOs.Responses
         public long Id { get; set; }
 
         public String Status { get; set; }
+
+        public String Nome { get; set; }
+
+        public String Descricao { get; set; }
+
+        public long Valor { get; set; }
+
+        public String Responsavel { get; set; }
     }
 }
